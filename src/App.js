1e01b4ffe38d6118f91d6afb256a05e3ba39d615
Anwar Louis <smmaniac@hotmail.co.uk>
2024-03-06 20:29:38 +0000

    Blur and unblur names with submit form

--- src/App.js
+++ src/App.js
@@ -2,14 +2,44 @@ import logo from './logo.svg';
 import './App.css';
 import PokemonName from './components/PokemonName';
 import pokemonNames from './data/pokemon_names.json';
+import { useRef, useState } from 'react';
 
 function App() {
+  // attempts array is populated when user submits names through form
+  const [attempts, setAttempts] = useState([]);
+  // allows you to read the input form
+  const inputRef = useRef();
+  function onSubmit(event) {
+    // disable default handling
+    event.preventDefault();
+    // read the input name from form
+    const name = inputRef.current.value;
+    // add name to list of attempts
+    setAttempts((attempts) => [...attempts, name]);
+    // clear input box
+    inputRef.current.value = '';
+  }
+
   return (
     <div className="App">
       <div className="nameGrid">
+        {/* displays all pokemon names from mapped data */}
         {pokemonNames.map((name) => (
-          <PokemonName name={name}></PokemonName>
+          // PokeName component imported
+          <PokemonName
+            name={name}
+            // if the name has been attempted, the name will be unblurred
+            isBlurred={!attempts.includes(name)}
+          ></PokemonName>
         ))}
+      </div>
+      <div>
+        {/* for the form the user types their attempts into */}
+        <form onSubmit={onSubmit}>
+          {/* text is read by inputRef to be used in the onSubmit */}
+          <input type="text" ref={inputRef} placeholder="guess" />
+          <input class="styled" type="submit" value="Submit?" />
+        </form>
       </div>
     </div>
   );
--- src/components/PokemonName.jsx
+++ src/components/PokemonName.jsx
@@ -3,6 +3,7 @@ import styles from './PokemonName.module.css';
 export default function PokemonName(props) {
   return (
     <div className={styles.tile}>
+      {/* if isBlurred is true, styles.blur will be applied, otherwise nothing will be applied */}
       <span className={props.isBlurred ? styles.blur : ''}>{props.name}</span>
     </div>
   );
