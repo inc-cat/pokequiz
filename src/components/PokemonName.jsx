c6efedc7c75f4184fd0033f39b5fa71e8af56e25
Anwar Louis <smmaniac@hotmail.co.uk>
2024-03-06 19:44:32 +0000

    Add blur property to Pokemon name

--- src/components/PokemonName.jsx
+++ src/components/PokemonName.jsx
@@ -1,5 +1,9 @@
 import styles from './PokemonName.module.css';
 
 export default function PokemonName(props) {
-  return <div className={styles.tile}>{props.name}</div>;
+  return (
+    <div className={styles.tile}>
+      <span className={props.isBlurred ? styles.blur : ''}>{props.name}</span>
+    </div>
+  );
 }
