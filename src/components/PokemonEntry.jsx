4ab910a8d3c959df36dedd98f1de06590a7b3a44
Anwar Louis <smmaniac@hotmail.co.uk>
2024-04-10 23:04:22 +0100

    Add pokemon sprites to the entries

--- src/components/PokemonEntry.jsx
+++ src/components/PokemonEntry.jsx
@@ -7,6 +7,11 @@ export default function PokemonEntry(props) {
       <span className={styles.id}>#{pokemon.id}</span>
       {/* if isBlurred is true, styles.blur will be applied, otherwise nothing will be applied */}
       <span className={props.isBlurred ? styles.blur : ''}>{pokemon.name}</span>
+      <span
+        className={(props.isBlurred ? styles.blur : '') + ' ' + styles.sprite}
+      >
+        <img src={pokemon.img}></img>
+      </span>
     </div>
   );
 }
